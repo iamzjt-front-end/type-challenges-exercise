b36bd2d7cd8734b4101b204e0b0ec7c90a74e528
iamzjt-front-end <iamzjt@foxmail.com>
2023-06-06 21:29:16 +0800

    ✨ fix: 修复之前的解答，00011-easy-tuple-to-object

--- type-challenges/00011-easy-tuple-to-object/template.ts
+++ type-challenges/00011-easy-tuple-to-object/template.ts
@@ -1,3 +1,11 @@
-type TupleToObject<T extends readonly any[]> = {
+type TupleToObject<T extends readonly (string | number)[]> = {
   [P in T[number]]: P
 }
+
+// * @ts-expect-error
+// * 这一句注释的意思是，期望底下的这段代码抛出错误，否则这句注释就会报错
+// * -> 在此题的意思就是，需要限制 TupleToObject 的参数: tuple 内必须是 string 或 number
+
+// + 当然看网上其他一些解答是用 PropertyKey 来限制的，这样就可以兼容 symbol 了
+// + PropertyKey = string | number | symbol
+// + 但是我觉得这样就不是 tuple 了，因为 symbol 是不会有顺序的
--- type-challenges/00011-easy-tuple-to-object/test-cases.ts
+++ type-challenges/00011-easy-tuple-to-object/test-cases.ts
@@ -10,4 +10,5 @@ type cases = [
   Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>>,
 ]
 
+// @ts-expect-error
 type error = TupleToObject<[[1, 2], {}]>
