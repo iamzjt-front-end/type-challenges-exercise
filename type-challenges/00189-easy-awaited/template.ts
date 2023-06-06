593bb39c579519bcf30e3b3e0d8a5db7db54bd9f
iamzjt-front-end <iamzjt@foxmail.com>
2023-06-06 10:46:57 +0800

    ✨ feat: pass 00189-easy-awaited

--- type-challenges/00189-easy-awaited/template.ts
+++ type-challenges/00189-easy-awaited/template.ts
@@ -1,1 +1,7 @@
-type MyAwaited<T> = any
+type MyAwaited<T extends Promise<any> | PromiseLike<any>> = T extends Promise<infer U>
+                                         ? U extends Promise<any>
+                                           ? MyAwaited<U>
+                                           : U
+                                         : T extends PromiseLike<infer P>
+                                           ? P
+                                           : never
