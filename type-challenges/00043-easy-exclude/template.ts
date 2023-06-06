0f8414ee8e69c7c825d971e87febeac288eb33ff
iamzjt-front-end <iamzjt@foxmail.com>
2023-06-06 10:11:50 +0800

    ✨ feat: pass 00043-easy-exclude

--- type-challenges/00043-easy-exclude/template.ts
+++ type-challenges/00043-easy-exclude/template.ts
@@ -1,1 +1,12 @@
-type MyExclude<T, U> = any
+type MyExclude<T, U> = T extends U ? never : T
+
+// * 当传入的被检查类型是联合类型的时候，类型检查过程会被分解为多个分支进行
+
+// T extends U ? X : Y
+// T => A | B | C
+// 假如 T 是联合类型 A ｜ B ｜ C
+// A | B | C extends U ? X : Y  =>
+// (A extends U ? X : Y) | (B extends U ? X : Y) | (C extends U ? X : Y)
+
+// * 内置类型 Exclude 用于删除类型集合中的指定类型
+// * 此案例就是实现一个Exclude
