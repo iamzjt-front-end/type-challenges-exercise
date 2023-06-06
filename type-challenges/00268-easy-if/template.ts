4287305a452f347ac2d2fd4971d32491c82d262b
iamzjt-front-end <iamzjt@foxmail.com>
2023-06-06 20:52:28 +0800

    ✨ feat: pass 00268-easy-if

--- type-challenges/00268-easy-if/template.ts
+++ type-challenges/00268-easy-if/template.ts
@@ -1,1 +1,5 @@
-type If<C, T, F> = any
+type If<C extends boolean, T, F> = C extends true ? T : F
+
+// * @ts-expect-error
+// * 这一句注释的意思是，期望底下的这段代码抛出错误，否则这句注释就会报错
+// * -> 在此题的意思就是，需要限制 If 第一个参数的类型，只能是 boolean
