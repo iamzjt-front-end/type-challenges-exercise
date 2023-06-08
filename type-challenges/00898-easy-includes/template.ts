301c34c840e8c5b51e5da51015445e52c7cfd417
iamzjt-front-end <iamzjt@foxmail.com>
2023-06-08 13:16:04 +0800

    ✨ feat: finish 00898-easy-includes

--- type-challenges/00898-easy-includes/template.ts
+++ type-challenges/00898-easy-includes/template.ts
@@ -1,1 +1,7 @@
-type Includes<T extends readonly any[], U> = any
+import { Equal } from "@type-challenges/utils";
+
+type Includes<T extends readonly any[], U> = T extends [infer F, ...infer R]
+                                             ? Equal<F, U> extends true
+                                               ? true
+                                               : Includes<R, U>
+                                             : false
