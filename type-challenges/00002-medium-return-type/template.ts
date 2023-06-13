68f2af50f825e4464a4a272741b3ad70a40e5b30
iamzjt-front-end <iamzjt@foxmail.com>
2023-06-13 11:19:06 +0800

    ✅ feat: pass 00002-medium-return-type

--- type-challenges/00002-medium-return-type/template.ts
+++ type-challenges/00002-medium-return-type/template.ts
@@ -1,1 +1,6 @@
-type MyReturnType<T> = any
+type MyReturnType<T extends Function> = T extends (...args: any[]) => infer P ? P : never;
+
+// - T 和模式类型做匹配，提取返回值到通过 infer 声明的局部变量 P 里返回。
+
+// - 参数类型可以是任意类型，也就是 any[]。
+// - 但是注意，这里不能用 unknown，因为涉及到参数的逆变性质。
