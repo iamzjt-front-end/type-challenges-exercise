59b7a03749eb59b5026242970471a1e5dc702489
iamzjt-front-end <iamzjt@foxmail.com>
2023-06-15 17:38:26 +0800

    ✅ feat: pass 00016-medium-pop

--- type-challenges/00016-medium-pop/template.ts
+++ type-challenges/00016-medium-pop/template.ts
@@ -1,1 +1,6 @@
-type Pop<T extends any[]> = any
+type Pop<T extends any[]> = T extends [...infer R, infer L] ? R : T
+
+// * 附加题
+// - type MyShift<T extends any[]> = T extends [infer F, ...infer R] ? F : never
+// - type Push<T extends any[], K> = [...T, K]
+// - type MyUnshift<T extend any[], K> = [K, ...T]
