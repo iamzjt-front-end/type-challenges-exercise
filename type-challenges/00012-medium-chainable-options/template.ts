0af17706a3369d7b185a65ee10fbaaaa21433b0e
iamzjt-front-end <iamzjt@foxmail.com>
2023-07-10 11:44:42 +0800

    ✅ feat: pass 00012-medium-chainable-options

--- type-challenges/00012-medium-chainable-options/template.ts
+++ type-challenges/00012-medium-chainable-options/template.ts
@@ -1,4 +1,7 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<T extends Record<string, any> = {}> = {
+  option: <K extends string, V>(
+      key: K extends keyof T ? never : K,
+      value: V,
+  ) => Chainable<Omit<T, K> & Record<K, V>>
+  get(): T
 }
